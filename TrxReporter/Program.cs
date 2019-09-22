7f0e98bdb49cf45b3101b15b6508b1508ef411a5
Steven M. Cohn <steven_cohn@waters.com>
2019-09-22 07:56:34 -0400

    add output argument

--- TrxReporter/Program.cs
+++ TrxReporter/Program.cs
@@ -10,25 +10,46 @@
 	class Program
 	{
 
-		private const string XsltFile = "TrxReporter.xslt";
-		private const string OutputFileExt = ".html";
+		private const string XSLT = "TrxReporter.xslt";
+		private const string HTMLExt = ".html";
 
 
 		static void Main(string[] args)
 		{
-			if (args.Any() == false)
+			if (!args.Any())
 			{
-				Console.WriteLine("No trx file, TrxReporter.exe <filename>");
+				Console.WriteLine("Usage: dotnet .\\TrxReporter.dll <trx-path> <output-path>");
 				return;
 			}
 
-			Console.WriteLine($"... transforming {args[0]}");
+			var trxPath = args[0];
+			string outPath;
+			if (args.Length > 1)
+			{
+				if (args[1].EndsWith(HTMLExt, StringComparison.InvariantCultureIgnoreCase))
+				{
+					outPath = args[1];
+				}
+				else
+				{
+					outPath = Path.Combine(args[1], Path.GetFileName(trxPath) + HTMLExt);
+				}
+			}
+			else
+			{
+				outPath = Path.Combine(
+					Path.GetDirectoryName(Path.GetFullPath(trxPath)),
+					Path.GetFileName(trxPath) + HTMLExt
+					);
+			}
 
-			Transform(args[0], PrepareXsl());
+			Console.WriteLine($"... transforming {trxPath}");
+
+			Transform(trxPath, outPath, PrepareXsl());
 		}
 
 
-		private static void Transform(string fileName, XmlDocument xsl)
+		private static void Transform(string fileName, string outPath, XmlDocument xsl)
 		{
 			var compiled = new XslCompiledTransform(true);
 			compiled.Load(xsl, new XsltSettings(true, true), null);
@@ -37,12 +58,12 @@
 			args.AddExtensionObject("urn:Pens", new Pens());
 
 			Console.WriteLine("... transforming");
-			using (var writer = new StreamWriter(fileName + OutputFileExt))
+			using (var writer = new StreamWriter(outPath))
 			{
 				compiled.Transform(fileName, args, writer);
 			}
 
-			Console.WriteLine($"... {fileName + OutputFileExt}");
+			Console.WriteLine($"... {outPath}");
 		}
 
 
@@ -50,7 +71,7 @@
 		{
 			Console.WriteLine("... loading xslt template");
 			var doc = new XmlDocument();
-			doc.Load(ResourceReader.StreamFromResource(XsltFile));
+			doc.Load(ResourceReader.StreamFromResource(XSLT));
 			MergeCss(doc);
 			MergeJavaScript(doc);
 			return doc;
